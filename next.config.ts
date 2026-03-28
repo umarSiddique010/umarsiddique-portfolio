ca4d6d9673cf41c38a50b68d8864028fd0ff9a7b
Md Umar Siddique <us70763@gamil.com>
2026-03-28 21:56:47 +0530

    chore: add application/pdf content-type header for pdf files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   compress: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*.pdf',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/pdf',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
